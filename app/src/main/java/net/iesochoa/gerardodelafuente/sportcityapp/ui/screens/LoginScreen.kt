6cc0cf04293ec9e00d7b5c064f7df54cac49bc44
Gerardo de la fuente <gerdeluri@alu.edu.gva.es>
2026-01-04 18:52:33 +0100

    Pantalla login

--- app/src/main/java/net/iesochoa/gerardodelafuente/sportcityapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/net/iesochoa/gerardodelafuente/sportcityapp/ui/screens/LoginScreen.kt
@@ -1,8 +1,11 @@
 package net.iesochoa.gerardodelafuente.sportcityapp.ui.screens
+import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,9 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.iesochoa.gerardodelafuente.sportcityapp.ui.theme.ColorBackground
+import net.iesochoa.gerardodelafuente.sportcityapp.ui.theme.ColorPrimary
+import net.iesochoa.gerardodelafuente.sportcityapp.ui.theme.ColorTextPrimary
+import net.iesochoa.gerardodelafuente.sportcityapp.ui.theme.ColorTextSecondary
 
 
 @Composable
@@ -29,6 +38,31 @@ fun loginScrenn(){
                 .padding(horizontal = 24.dp, vertical = 32.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                text = stringResource(net.iesochoa.gerardodelafuente.sportcityapp.R.string.bienvenido),
+                color = ColorTextPrimary,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = stringResource(net.iesochoa.gerardodelafuente.sportcityapp.R.string.subtitulo_bienvenida),
+                color= ColorTextSecondary,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+
+
+
+
 
         }
     }
